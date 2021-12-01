0753218072c7f0cd0ed23893e9f86af1d5de1ea8
BinarySky10 <yilingxiankong@hotmail.com>
2021-12-01 21:41:44 +0800

    初次提交

---
+++ src/components/PineHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="pine-header-nav">
+    <div class="pine-header-layout">
+      <div class="pine-logo">松树板</div>
+      <div class="pine-search">
+        <input type="text" placeholder="搜索">
+      </div>
+      <div class="">
+        工具
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PineHeader',
+  
+}
+</script>
+
+<style scoped lang="scss">
+#pine-header-nav{
+  //位
+  position: fixed;
+  top: 0;
+  left: 0;
+  
+  //大
+  width: 100%;
+  height: 44px;
+  //文
+
+  //景
+
+  //动
+  background: gold;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <pine-header></pine-header>
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+  </div>
+</template>
+
+<script>
+import PineHeader from '@/components/PineHeader.vue'
+export default {
+  name: 'Home',
+  components: {
+    PineHeader
+  }
+}
+</script>
